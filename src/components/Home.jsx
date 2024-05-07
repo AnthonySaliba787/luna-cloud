adcb4d2e75d6052191dbf38de5067ea16161f747
Anthony Saliba <anthonysaliba93@gmail.com>
2024-05-07 21:24:47 +0300

    Implement new homepage section and fix bugs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import { ReactTyped } from "react-typed";
 import Cloud from "../assets/cloud.png";
+import { FaEye, FaLightbulb, FaLock } from "react-icons/fa";
+import { IoIosSpeedometer } from "react-icons/io";
 
 function Home() {
   return (
     <>
       <div>
-        <div className="max-w-[1000px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <div className="max-w-[1000px] h-screen mx-auto text-center flex flex-col justify-center items-center">
           <img
             className="w-80 mx-auto my-[-3rem]"
             src={Cloud}
@@ -14,7 +16,7 @@ function Home() {
           <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold py-2">
             For people who care about privacy.
           </h1>
-          <p className="text-1xl md:text-1xl text-blue-600">
+          <p className="text-1xl text-blue-600">
             Cloud storage that guarantees{" "}
             <span>
               <ReactTyped
@@ -32,9 +34,45 @@ function Home() {
             </span>
             {""}
           </p>
-          <button className="w-[150px] md:w-[175px] lg:w-[200px] py-4 my-4 bg-blue-600 text-white  rounded-md mx-auto font-medium hover:bg-blue-500 active:bg-blue-400 duration-300">
-            Learn More
-          </button>
+          <button className="main-button">Learn More</button>
+        </div>
+        <div className="max-w-[1300px] grid md:grid-cols-2 gap-12 py-8 text-center mx-auto px-4">
+          <div className="card">
+            <FaLightbulb size={40} />
+            <h1 className="card-title">Innovative</h1>
+            <p className="card-p">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Temporibus deleniti voluptatibus at adipisci corporis nulla quidem
+              obcaecati ut sint nihil.
+            </p>
+          </div>
+          <div className="card">
+            <FaLock size={40} />
+            <h1 className="card-title">Secure</h1>
+            <p className="card-p">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
+              magnam assumenda esse cupiditate nemo debitis libero dolore minus,
+              eum qui.
+            </p>
+          </div>
+          <div className="card">
+            <IoIosSpeedometer size={40} />
+            <h1 className="card-title">Fast</h1>
+            <p className="card-p">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
+              magnam assumenda esse cupiditate nemo debitis libero dolore minus,
+              eum qui.
+            </p>
+          </div>
+          <div className="card">
+            <FaEye size={40} />
+            <h1 className="card-title">Private</h1>
+            <p className="card-p">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
+              magnam assumenda esse cupiditate nemo debitis libero dolore minus,
+              eum qui.
+            </p>
+          </div>
         </div>
       </div>
     </>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,25 +3,24 @@ import { NavLink } from "react-router-dom";
 function Nav() {
   return (
     <>
-      <div className="fixed w-full h-24 bg-[255, 255, 255, 0.8] flex items-center justify-between px-4 backdrop-blur-xl">
-        <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold px-2 md:px-4 lg:px-8">
+      <div className="fixed w-full h-24 bg-white/70 flex items-center justify-between px-4 backdrop-blur-xl border-b z-10">
+        <h1 className="w-full py-6 text-xl md:text-3xl lg:text-4xl font-bold px-2 md:px-4 lg:px-8">
           Luna Cloud
         </h1>
-        <div className="flex gap-4">
+        <div className="grid grid-cols-3 w-96 text-center justify-center items-center">
           <NavLink
             to={"/"}
             className={({ isActive }) =>
               isActive ? "text-blue-600 font-bold" : ""
             }
           >
-            <button className="w-[100px] md:w-[200px] py-8 font-medium hover:text-blue-400 duration-300">
-              Home
-            </button>
+            <button className="nav-button">Home</button>
           </NavLink>
           <NavLink>
-            <button className="w-[100px] md:w-[200px] py-8 font-medium hover:text-blue-400 duration-300">
-              About
-            </button>
+            <button className="nav-button">Pricing</button>
+          </NavLink>
+          <NavLink>
+            <button className="nav-button">About</button>
           </NavLink>
         </div>
       </div>
