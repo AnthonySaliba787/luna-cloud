f83f0fddda8717a793cdb94c19a43a3735128a79
Anthony Saliba <anthonysaliba93@gmail.com>
2024-05-08 12:14:40 +0300

    Implement the nopage page

--- src/components/NoPage.jsx
+++ src/components/NoPage.jsx
@@ -1,9 +1,11 @@
 function NoPage() {
   return (
     <>
-      {/* // TODO: Ensure the screen fills the whole viewport and optimize the text size to look good across devices */}
-      <div>
-        <h1>Error 404 - Page Not Found</h1>
+      <div className="max-w-[1000px] py-72 h-screen mx-auto text-center flex flex-col justify-center items-center">
+        <h1 className="text-3xl font-bold">Error 404 - Page Not Found</h1>
+        <p className="text-xl font-medium text-blue-500 py-4">
+          Please return to an available page through the navigation bar.
+        </p>
       </div>
     </>
   );
